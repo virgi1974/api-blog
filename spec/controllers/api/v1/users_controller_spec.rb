eb08e34347c91696cda7b5ca35e4db386eb7b3bf
virgilio1974 <virgifp@yahoo.es>
2016-02-16 14:55:57 +0100

    UNIT-TEST UsersController#show

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -22,4 +22,23 @@ describe Api::V1::UsersController do
     it { should respond_with 200 }
   end
 
+  describe "GET #show" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show ,id: @user.id
+      @user_response = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    it "response contains the users data" do
+      expect(@user_response[:email]).to eql @user.email
+      expect(@user_response[:auth_token]).to eql @user.auth_token
+      expect(@user_response[:password_digest]).to eql @user.password_digest
+    end
+    it "response adds all users" do
+      expect(@user_response.empty?).to be(false)
+    end
+
+    it { should respond_with 200 }
+  end
+
 end #describe Api::V1::UsersController 
