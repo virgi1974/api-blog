62fc8983ade9c20045df3c556908785c3d66f090
virgilio1974 <virgifp@yahoo.es>
2016-02-16 13:02:45 +0100

    TDD usermodel uniqueness+presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates :auth_token, presence: true
+  validates :auth_token, uniqueness: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,23 @@ describe User do
 
   subject { @user }
 
+# first test
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:auth_token) }
 
   it { should be_valid }
+
+# shoulda matchers in action
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should allow_value('example@domain.com').for(:email) }
+
+  it { should validate_presence_of(:password) }
+
+  it { should validate_presence_of(:auth_token) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+
 
 end
